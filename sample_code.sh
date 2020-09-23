ceeb906a4d9047961f57a445ea0c06e8ec3796bc
venkataramana94 <64878015+venkataramana94@users.noreply.github.com>
2020-09-23 22:00:14 +0530

    second commit

--- sample_code.sh
+++ sample_code.sh
@@ -6,5 +6,6 @@ echo "This is test from Linux Server"
 echo "This is an example of a script"
 echo "This is running in a build step and it shows changes"
 echo "this is webhook practice"
+echo "this is webhook practice123"
 SUM=$((FIRSTNUMBER + SECONDNUMBER))
 echo "$SUM"
